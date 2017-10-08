af74c32cbbcb3a87b8352b812a1f6cf57200e574
Lee Keitel <lee@keitel.xyz>
2017-10-07 22:06:06 -0500

    Expanded RPC test script with all available calls

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -4,6 +4,7 @@ import (
 	"flag"
 	"log"
 	"net"
+	"time"
 
 	"github.com/packet-guardian/pg-dhcp/rpcclient"
 )
@@ -31,17 +32,113 @@ func main() {
 		args = flag.Args()[1:]
 	}
 
+	t := &timer{}
+
 	if command == "device-test" {
 		mac, _ := net.ParseMAC(args[0])
+
+		t.start()
 		if err := client.Device().Register(mac); err != nil {
 			log.Fatal(err)
 		}
+		t.print()
 
-		d, err := client.Device().Get(mac)
+		t.start()
+		if err := getAndPrintDevice(client, mac); err != nil {
+			log.Fatal(err)
+		}
+		t.print()
+
+		t.start()
+		if err := client.Device().Blacklist(mac); err != nil {
+			log.Fatal(err)
+		}
+		t.print()
+
+		t.start()
+		if err := getAndPrintDevice(client, mac); err != nil {
+			log.Fatal(err)
+		}
+		t.print()
+
+		t.start()
+		if err := client.Device().Unregister(mac); err != nil {
+			log.Fatal(err)
+		}
+		t.print()
+
+		t.start()
+		if err := getAndPrintDevice(client, mac); err != nil {
+			log.Fatal(err)
+		}
+		t.print()
+
+		t.start()
+		if err := client.Device().RemoveBlacklist(mac); err != nil {
+			log.Fatal(err)
+		}
+		t.print()
+
+		t.start()
+		if err := getAndPrintDevice(client, mac); err != nil {
+			log.Fatal(err)
+		}
+		t.print()
+	} else if command == "network-test" {
+		list, err := client.Network().GetNameList()
 		if err != nil {
 			log.Fatal(err)
 		}
+		log.Printf("%#v\n", list)
+	} else if command == "lease-test" {
+		leases, err := client.Lease().GetAllFromNetwork("network1")
+		if err != nil {
+			log.Fatal(err)
+		}
+		for _, lease := range leases {
+			log.Printf("%#v\n", lease)
+		}
 
-		log.Printf("%#v\n", d)
+		lease, err := client.Lease().Get(net.ParseIP("10.0.2.12"))
+		if err != nil {
+			log.Fatal(err)
+		}
+		log.Printf("\n%#v\n", lease)
+
+		lease, err = client.Lease().Get(net.ParseIP("10.0.2.3"))
+		if err != nil {
+			log.Fatal(err)
+		}
+		log.Printf("\n%#v\n", lease)
+	} else if command == "stat-test" {
+		stats, err := client.Server().GetPoolStats()
+		if err != nil {
+			log.Fatal(err)
+		}
+		for _, stat := range stats {
+			log.Printf("%#v\n", stat)
+		}
 	}
+}
+
+func getAndPrintDevice(c *rpcclient.Client, mac net.HardwareAddr) error {
+	d, err := c.Device().Get(mac)
+	if err != nil {
+		return err
+	}
+
+	log.Printf("%#v\n", d)
+	return nil
+}
+
+type timer struct {
+	s time.Time
+}
+
+func (t *timer) start() {
+	t.s = time.Now()
+}
+
+func (t *timer) print() {
+	log.Printf("%s\n", time.Now().Sub(t.s).String())
 }
