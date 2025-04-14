13f861a8cf8e1ea35f2e2d7457d4499a3561dde6
Lee Keitel <lee@keitel.xyz>
2025-04-14 11:29:47 -0500

    Don't edit devices managed by PG

--- store/pgstore.go
+++ store/pgstore.go
@@ -145,15 +145,7 @@ func (s *PGStore) deviceBlacklisted(mac net.HardwareAddr) bool {
 }
 
 func (s *PGStore) PutDevice(d *models.Device) error {
-	if err := s.prepare(); err != nil {
-		return err
-	}
-
-	_, err := s.putDeviceStmt.Exec(
-		d.LastSeen.Unix(),
-		d.MAC.String(),
-	)
-	return err
+	return nil // We don't manage the devices, the management application does.
 }
 
 func (s *PGStore) DeleteDevice(d *models.Device) error {
